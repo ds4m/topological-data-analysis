2c93fa5dac74f7e9c62f060c0a8f4f7aeaac732c
Henry Adams <henry@Henrys-MacBook-Pro.local>
2018-07-27 11:29:47 -0600

    Adding and removing files

--- topological-feature-vectors/euler-curves/analyze_devan.py
+++ topological-feature-vectors/euler-curves/analyze_devan.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Analyzing and Classifying MNIST Dataset using euler characteristic curves
+Analyzing and Classifying Devanagari Characters Dataset using euler characteristic curves
 
-@author: Elchanan Solomon
+@author: Elchanan
 """
 
 import ECC
 import numpy as np
-
 #Sample 8 directions on the unit circle
 #directions = [(np.cos(x),np.sin(x)) for x in np.linspace(0,2*np.pi,8)]
 #some sample directions
 directions = [(1,0),(0,1),(-1,0),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1),(2,1),(2,-1),(-2,1),(-2,-1)]
+#directions = [(np.cos(x),np.sin(x)) for x in np.linspace(0,np.pi,4)]
 
 #Predict labels for 100 test images
-labels = ECC.predict_labels(mnist_train_data,mnist_train_labels,mnist_test_data[:100],directions)
+labels = ECC.predict_labels(devan_train_figures,devan_train_labels,devan_test_figures[:100],directions,1)
 #Display the score
-print(sum(labels == mnist_test_labels[:100]))
+print(sum(np.array(labels) == np.array(devan_test_labels[:100])))
 
